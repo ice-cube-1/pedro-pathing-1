2320b11720ffc974256017f6d326ba8dd4ac23b2
ice-cube-1 <alice.goward@spgs.org>
2026-04-20 01:27:01 +0100

    tuned constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,52 +1,58 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.Encoder;
-import com.pedropathing.ftc.localization.constants.DriveEncoderConstants;
+import com.pedropathing.ftc.localization.constants.ThreeWheelConstants;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
+            .headingPIDFCoefficients(new PIDFCoefficients(0.9,0.0,0.1,0.04))
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.08,0.0,0.0001,0.04))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.006,0.0,0.0,0.6,0.0))
+            .forwardZeroPowerAcceleration(-36.4121702774264)
+            .lateralZeroPowerAcceleration(-44.48100887726937)
+            .centripetalScaling(0.015)
             .mass(5);
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
             .rightFrontMotorName("rf")
-            .rightRearMotorName("rr")
-            .leftRearMotorName("lr")
+            .rightRearMotorName("rb")
+            .leftRearMotorName("lb")
             .leftFrontMotorName("lf")
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .xVelocity(49.72054942450339);
-
+            .yVelocity(39.575690497431395)
+            .xVelocity(38.92510387440863);
+    public static ThreeWheelConstants localizerConstants = new ThreeWheelConstants()
+            .forwardTicksToInches(0.0023583856160810848)
+            .strafeTicksToInches(0.002872550456680683)
+            .turnTicksToInches(0.00759113846)
+            .leftEncoder_HardwareMapName("lf")
+            .rightEncoder_HardwareMapName("rf")
+            .strafeEncoder_HardwareMapName("lb")
+            .leftEncoderDirection(Encoder.FORWARD)
+            .rightEncoderDirection(Encoder.REVERSE)
+            .strafeEncoderDirection(Encoder.REVERSE)
+            .leftPodY(10.0)
+            .rightPodY(-10.0)
+            .strafePodX(-12.0);
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
-
-    public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
-            .rightFrontMotorName("rf")
-            .rightRearMotorName("rr")
-            .leftRearMotorName("lr")
-            .leftFrontMotorName("lf")
-            .robotWidth(16)
-            .robotLength(12)
-            .forwardTicksToInches(0.0058724387777206385)
-            .strafeTicksToInches(0.003374368161656415)
-            .turnTicksToInches(0.006146212569907357)
-            .leftFrontEncoderDirection(Encoder.REVERSE)
-            .leftRearEncoderDirection(Encoder.REVERSE)
-            .rightFrontEncoderDirection(Encoder.FORWARD)
-            .rightRearEncoderDirection(Encoder.FORWARD);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
-                .driveEncoderLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
                 .mecanumDrivetrain(driveConstants)
+                .threeWheelLocalizer(localizerConstants)
                 .build();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/OdometryWheelsConfig.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class OdometryWheelsConfig extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        waitForStart();
+
+        DcMotorEx[] wheels = new DcMotorEx[] {
+                initOdoWheel("lf", DcMotorSimple.Direction.REVERSE),
+                initOdoWheel("rf", DcMotorSimple.Direction.FORWARD),
+                initOdoWheel("lb", DcMotorSimple.Direction.REVERSE),
+                initOdoWheel("rb", DcMotorSimple.Direction.FORWARD)
+        };
+
+        while (opModeIsActive()) {
+
+            if (gamepad1.left_trigger > 0.5) {
+                wheels[0].setPower(0.5);
+            } else {
+                wheels[0].setPower(0.0);
+            }
+
+            if (gamepad1.right_trigger > 0.5) {
+                wheels[1].setPower(0.5);
+            } else {
+                wheels[1].setPower(0.0);
+            }
+
+            if (gamepad1.left_bumper) {
+                wheels[2].setPower(0.5);
+            } else {
+                wheels[2].setPower(0.0);
+            }
+
+            if (gamepad1.right_bumper) {
+                wheels[3].setPower(0.5);
+            } else {
+                wheels[3].setPower(0.0);
+            }
+
+            telemetry.addLine("LT - LF, RT - RF, LB - LB, RB - RB");
+            telemetry.addData("LF encoder", wheels[0].getCurrentPosition());
+            telemetry.addData("RF encoder", wheels[1].getCurrentPosition());
+            telemetry.addData("LB encoder", wheels[2].getCurrentPosition());
+            telemetry.addData("RB encoder", wheels[3].getCurrentPosition());
+            telemetry.update();
+        }
+    }
+
+    private DcMotorEx initOdoWheel(String name, DcMotorSimple.Direction direction) {
+        DcMotorEx motor = hardwareMap.get(DcMotorEx.class, name);
+
+        motor.setDirection(direction);
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        return motor;
+    }
+}
